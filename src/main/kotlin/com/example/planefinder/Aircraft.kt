ad62fe6474672310a145100231b473451f30cf47
dsyang <dsyang.tech@gmail.com>
2024-01-01 17:38:28 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/planefinder/Aircraft.kt
@@ -0,0 +1,4 @@
+package com.example.planefinder
+
+class Aircraft {
+}
---
+++ src/main/kotlin/com/example/planefinder/PlanefinderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.planefinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanefinderApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlanefinderApplication>(*args)
+}
